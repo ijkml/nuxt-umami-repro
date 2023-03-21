b0068879bc01b88897da4e07f116e8bf85fd1918
ijkml <me.mlaure@gmail.com>
2023-03-21 18:44:07 +0100

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,63 @@
+<script setup>
+// onMounted(() => {});
+
+function testView() {
+  umTrackView();
+}
+function testEvent() {
+  umTrackEvent('stackblitz-event', { type: 'click', position: 'left' });
+}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="page-root">
+    <div class="page-container">
+      <h1>Nuxt Umami</h1>
+      <br>
+      <button @click="testEvent">
+        Run trackEvent
+      </button>
+      <button @click="testView">
+        Run trackView
+      </button>
+      <a href="https://ml-umami.netlify.app/share/8o0OFImY/Umami%20Next" target="_blank" rel="noopener noreferrer">See Preview</a>
+    </div>
   </div>
 </template>
+
+<style src="./reset.css"></style>
+
+<style scoped>
+.page-root {
+  background: linear-gradient(to bottom right, burlywood, aliceblue);
+  min-height: 100vh;
+  padding: 40px;
+  display: grid;
+  place-items: center;
+  text-align: center;
+  font-family: "Source Sans Pro", system-ui, -apple-system, Ubuntu, sans-serif;
+}
+
+h1 {
+  font-size: x-large;
+  font-weight: 600;
+}
+
+:is(button, a) {
+  padding: 8px 12px;
+  margin: 16px;
+  outline: none;
+  cursor: pointer;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+  width: auto;
+  height: auto;
+  background-color: whitesmoke;
+  transition: all 350ms cubic-bezier(0.445, 0.05, 0.55, 0.95);
+}
+
+:is(button, a):is(:hover, :focus-visible) {
+  background-color: rgb(209, 209, 209);
+  outline: none;
+}
+</style>
