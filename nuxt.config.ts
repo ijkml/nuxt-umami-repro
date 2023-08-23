ba3a068de2abb591f1b14be470612d6a509afc15
ML <me.mlaure@gmail.com>
2023-08-23 16:12:23 +0100

    update umami config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   extends: ['nuxt-umami'],
   appConfig: {
     umami: {
       host: 'https://ml-umami.netlify.app',
-      id: 'ba4c9424-c4b7-48df-b66d-4213730673e5',
+      id: '82d2c873-ad36-4ea3-9829-e283bc681a81',
+      version: 2,
+      debug: true,
     },
   },
 });
