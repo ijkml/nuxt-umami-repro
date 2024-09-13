5746ba618e1e49fd9949cd2a99cda5eed772595b
ML <me.mlaure@gmail.com>
2024-09-13 13:23:18 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
   modules: ['nuxt-umami'],
 
   umami: {
-    enabled: false,
-    host: 'https://savory.vercel.app/',
-    id: '84cc2d28-8689-4df0-b575-2202e34a75aa',
-    proxy: 'cloak',
+    // enabled: false,
+    host: 'https://savory.ijkml.dev/',
+    id: '94c6eb8c-646d-41ff-8b5b-a924ce2b4111',
     useDirective: true,
     logErrors: true,
+  },
+
+  appConfig: {
+    shareUrl: 'https://savory.ijkml.dev/share/xj2RHnDuAD8khsui/localhost',
   },
 
   css: ['@/assets/reset.css'],
